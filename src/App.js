f3e0bdfc505a913a8469ad6276ae63568a833e06
Ajeet <ar-aishwarya.ajeet@rakuten.com>
2020-07-01 11:12:45 +0530

    my project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Doughnut } from "react-chartjs-2";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const state = {
+  labels: ["Available SKUs", "Failed SKUs"],
+  datasets: [
+    {
+      label: "SKUs",
+      backgroundColor: ["#27c48d", "#d1192b"],
+      hoverBackgroundColor: ["#31cc77", "#990c24"],
+      data: [957, 43],
+    },
+  ],
+};
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Doughnut
+          data={state}
+          height={40}
+          options={{
+            title: {
+              display: true,
+              text: "Pricing Intelligence",
+              fontSize: 20,
+            },
+            legend: {
+              labels: {
+                usePointStyle: true,
+              },
+              position: "right",
+            },
+            cutoutPercentage: 60,
+          }}
+        />
+      </div>
+    );
+  }
 }
-
-export default App;
